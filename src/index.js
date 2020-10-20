eb39964d04c4baedcdda95bd174fbfdfb17a3ea2
Helen-Scafander <khaletskayahelen@yandex.ru>
2020-10-20 23:53:03 +0400

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets) {
+  let i = 0;
+  let pair = '';
+  for (let j = 0; j < str.length*3; j++) {
+    while (i < brackets.length) {
+    pair = brackets[i].join('');
+    while (str.indexOf(pair) >= 0) {
+      str = str.replace(pair, "");
+    }
+    i++
+  }
+    i = 0;
+  }
+  if (str == '') {
+    return true;
+  } else return false;
 }
